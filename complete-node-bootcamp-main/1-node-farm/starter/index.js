42cf2690c8921b9e853b6068d54f599aa46a15ee
Tim Johnson <tjohnson@2ndswing.com>
2020-12-03 06:29:51 -0800

    create node package

--- complete-node-bootcamp-main/1-node-farm/starter/index.js
+++ complete-node-bootcamp-main/1-node-farm/starter/index.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
+const replaceTemplate = require('./modules/replaceTemplate')
+
 // // Blocking, sychronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 // console.log(textIn)
@@ -41,21 +43,7 @@ const url = require('url')
 //
 ////
 
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%IMAGE%}/g, product.image)
-  output = output.replace(/{%QUANTITY%}/g, product.quantity)
-  output = output.replace(/{%PRICE%}/g, product.price)
-  output = output.replace(/{%DESCRIPTION%}/g, product.quantity)
-  output = output.replace(/{%ID%}/g, product.id)
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-  output = output.replace(/{%FROM%}/g, product.from)
 
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, `not-organic`)
-
-  return output
-}
 
 const overviewTemplateHTML = fs.readFileSync(
   `${__dirname}/templates/overview-template.html`,
---
+++ complete-node-bootcamp-main/1-node-farm/starter/modules/replaceTemplate.js
@@ -0,0 +1,15 @@
+module.exports = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%DESCRIPTION%}/g, product.quantity)
+  output = output.replace(/{%ID%}/g, product.id)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%FROM%}/g, product.from)
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, `not-organic`)
+
+  return output
+}
