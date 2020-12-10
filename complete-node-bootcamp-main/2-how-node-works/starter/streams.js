87f7061dccb78f1b8fae012ceb956686103be206
pavementii@gmail.com <pavementii@gmail.com>
2020-12-10 06:44:33 -0800

    working with streams initial commit

---
+++ complete-node-bootcamp-main/2-how-node-works/starter/streams.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const server = require('http').createServer()
+
+server.on('request', (req, res) => {
+    // Solution 1
+    fs.readFile('test-file.txt', (err, data)=> {
+        if(err) console.log(err)
+        res.end(data)
+    })
+})
+
+server.listen(8000,'127.0.0.1', ()=>{
+    console.log('Listening...');
+    
+})
