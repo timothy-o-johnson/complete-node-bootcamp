4a6076201e08fed7b9af3436bbc6a4299dcc2d0b
pavementii@gmail.com <pavementii@gmail.com>
2020-12-11 19:35:40 -0800

    proving that node is an IIF

---
+++ complete-node-bootcamp-main/2-how-node-works/starter/modules.js
@@ -0,0 +1,7 @@
+// console.log('arguments: ', arguments);
+const args = Array.from(arguments)
+
+console.log('args',args);
+const wrapperFunction = require('module').wrapper 
+
+console.log('wrapperFunction', wrapperFunction);
