0ecfd2577bdf7479afe7d77fdb80c8d77ee8d761
pavementii@gmail.com <pavementii@gmail.com>
2020-12-11 17:18:54 -0800

    create a stream and error response

--- complete-node-bootcamp-main/2-how-node-works/starter/streams.js
+++ complete-node-bootcamp-main/2-how-node-works/starter/streams.js
@@ -3,13 +3,30 @@ const server = require('http').createServer()
 
 server.on('request', (req, res) => {
     // Solution 1
-    fs.readFile('test-file.txt', (err, data)=> {
-        if(err) console.log(err)
-        res.end(data)
+    // fs.readFile('test-file.txt', (err, data)=> {
+    //     if(err) console.log(err)
+    //     res.end(data)
+    // })
+
+    // Solution 2
+    const readable = fs.createReadStream('tesst-file.txt')
+    readable.on('data', chunk =>{
+        res.write(chunk)
+    })
+
+    readable.on('end', () =>{
+        res.end()
+    })
+
+    readable.on("error", err =>{
+        console.log(err);
+        res.statusCode = 500;
+        res.end('File not found!!!! :(')
+        
     })
 })
 
 server.listen(8000,'127.0.0.1', ()=>{
     console.log('Listening...');
     
-})
+})  
