b5d065af586accbc9e5c83c601aeb02f703c82f1
pavementii@gmail.com <pavementii@gmail.com>
2020-12-11 19:51:27 -0800

    practice importing/exporting using module.exports

--- complete-node-bootcamp-main/2-how-node-works/starter/modules.js
+++ complete-node-bootcamp-main/2-how-node-works/starter/modules.js
@@ -1,7 +1,18 @@
-// console.log('arguments: ', arguments);
-const args = Array.from(arguments)
+// // console.log('arguments: ', arguments);
+// const args = Array.from(arguments)
 
-console.log('args',args);
-const wrapperFunction = require('module').wrapper 
+// console.log('args',args);
+// const wrapperFunction = require('module').wrapper 
 
-console.log('wrapperFunction', wrapperFunction);
+// console.log('wrapperFunction', wrapperFunction);
+
+// module.exports: for exporting just one item
+const C = require('./test-module-1') // no need for '.js'
+
+const calculator = new C
+console.log(C.toString());
+
+console.log(calculator.multiply(5,2));
+console.log(calculator.add(5,2));
+console.log(calculator.divide(5,2.5));
+
