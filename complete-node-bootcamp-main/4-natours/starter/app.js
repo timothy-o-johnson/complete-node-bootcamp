4d493621645d3b5a1e7c8997bc1741b29632655b
pavementii@gmail.com <pavementii@gmail.com>
2020-12-18 17:54:33 -0600

    start natours project

---
+++ complete-node-bootcamp-main/4-natours/starter/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) =>{
+
+    res.status(200).send('Hello from the server side')
+})
+
+app.listen(port, ()=>{
+    console.log(`App running on port ${port}...`);
+    
+})
+
