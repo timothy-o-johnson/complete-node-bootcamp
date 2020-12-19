4180ab85b5f3268f3a8739fe878a4f2a111ee924
pavementii@gmail.com <pavementii@gmail.com>
2020-12-19 13:54:04 -0600

    attach middleware to posting practice data

--- complete-node-bootcamp-main/4-natours/starter/app.js
+++ complete-node-bootcamp-main/4-natours/starter/app.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -23,11 +25,18 @@ console.log(`tours ${JSON.stringify(tours)}`)
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
-    reslts: tours.length,
+    results: tours.length,
     data: {
       tours
     }
   })
+})
+
+app.post('/api/v1/tours', (req, res) =>{
+    var body = JSON.stringify(req.body)
+    console.log(`req.body:\n ${body}`);
+    res.status(200).send('hello, data has been posted')
+
 })
 
 app.listen(port, () => {
