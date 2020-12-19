9f9c312ad0feabe7bd7c7cac81a858e34f05fd95
pavementii@gmail.com <pavementii@gmail.com>
2020-12-19 13:12:53 -0600

    send tour data via get request

--- complete-node-bootcamp-main/4-natours/starter/app.js
+++ complete-node-bootcamp-main/4-natours/starter/app.js
@@ -1,15 +1,35 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) =>{
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' })
+// })
 
-    res.status(200).send('Hello from the server side')
+// app.post('/', (req, res) => {
+//   res.send(`you can post to this endpoint...`)
+// })
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+console.log(`tours ${JSON.stringify(tours)}`)
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    reslts: tours.length,
+    data: {
+      tours
+    }
+  })
 })
 
-app.listen(port, ()=>{
-    console.log(`App running on port ${port}...`);
-    
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`)
 })
-
