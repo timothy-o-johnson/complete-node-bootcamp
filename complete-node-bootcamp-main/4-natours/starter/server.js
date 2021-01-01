741ecf82e821d2ed0cd2c2721054c99391f80707
Tim Johnson <tjohnson@2ndswing.com>
2021-01-01 09:44:57 -0500

    connect to mongoose

--- complete-node-bootcamp-main/4-natours/starter/server.js
+++ complete-node-bootcamp-main/4-natours/starter/server.js
@@ -1,9 +1,24 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-dotenv.config({path: './config.env'})
+dotenv.config({ path: './config.env' })
 
 const app = require('./app')
 
 // console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+)
+
+mongoose.connect(DB, {
+  usedNewUrlParse: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(con =>{
+  console.log(con.connections)
+  console.log('DB connection successful')
+  
+})
 
 const port = process.env.PORT || 3000
 const setUpListener = () => {
