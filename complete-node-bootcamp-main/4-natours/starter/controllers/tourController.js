90f5b0d1bce9638dea13042ad0029012a3995a02
pavementii@gmail.com <pavementii@gmail.com>
2021-01-19 07:07:02 -0500

    limit number of fields returned

--- complete-node-bootcamp-main/4-natours/starter/controllers/tourController.js
+++ complete-node-bootcamp-main/4-natours/starter/controllers/tourController.js
@@ -79,6 +79,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt')
     }
 
+    // 3) Field limiting
+    if(req.query.fields){
+      const fields = req.query.fields.split(',').join(' ')
+      query = query.select(fields)
+    } else {
+      query = query.select('-__v')
+    }
+
     // var filterExample = { difficult: 'easy', duration: { $gte: 5 } }
 
     // const query =  Tour.find(
