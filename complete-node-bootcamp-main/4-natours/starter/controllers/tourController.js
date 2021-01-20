27100cff7d34035819ea4bde1b464472cbe8aa9f
pavementii@gmail.com <pavementii@gmail.com>
2021-01-20 07:54:46 -0500

    add aliasing

--- complete-node-bootcamp-main/4-natours/starter/controllers/tourController.js
+++ complete-node-bootcamp-main/4-natours/starter/controllers/tourController.js
@@ -28,6 +28,13 @@ exports.addATour = async (req, res) => {
   }
 }
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5'
+  req.query.sort = '-ratingsAverage,price'
+  req.query.fields = 'name,price,ratingsAverage,difficulty'
+  next()
+}
+
 exports.deleteATour = async (req, res) => {
   try {
     const id = req.params.id
@@ -62,25 +69,24 @@ exports.getAllTours = async (req, res) => {
     // 1B) Advance Filtering
     let queryStr = JSON.stringify(queryObj)
 
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}` )
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
     console.log('queryStr', queryStr)
 
     const newQueryObj = JSON.parse(queryStr)
 
     let query = Tour.find(newQueryObj)
 
-
     // 2) Sorting
-    if(req.query.sort){
+    if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ')
       query = query.sort(sortBy)
       // sort('price ratingsAverage)
-    } else{
+    } else {
       query = query.sort('-createdAt')
     }
 
     // 3) Field limiting
-    if(req.query.fields){
+    if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ')
       query = query.select(fields)
     } else {
@@ -90,13 +96,13 @@ exports.getAllTours = async (req, res) => {
     // 4) Pagination
     const page = req.query.page * 1 || 1
     const limit = req.query.limit * 1 || 100
-    const skip = (page -1) * limit
-    // page=2&limit=10, 1-10, page 1, 11-20,page 2, 21-30, page 3 
+    const skip = (page - 1) * limit
+    // page=2&limit=10, 1-10, page 1, 11-20,page 2, 21-30, page 3
     query = query.skip(skip).limit(limit)
 
-    if(req.query.page){
+    if (req.query.page) {
       const numTours = await Tour.countDocuments()
-      if(skip >= numTours) throw new Error('This page does not exist')
+      if (skip >= numTours) throw new Error('This page does not exist')
     }
 
     // var filterExample = { difficult: 'easy', duration: { $gte: 5 } }
@@ -128,8 +134,8 @@ exports.getAllTours = async (req, res) => {
       }
     })
   } catch (err) {
-    console.log('error:', error);
-    
+    console.log('error:', error)
+
     res.status(404).json({
       status: 'fail',
       message: err
