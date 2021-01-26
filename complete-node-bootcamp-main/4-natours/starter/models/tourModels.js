b4fc1d14a725a074ac7eae060a6cae14da71acdc
pavementii@gmail.com <pavementii@gmail.com>
2021-01-26 08:09:32 -0500

    add virtual fields

--- complete-node-bootcamp-main/4-natours/starter/models/tourModels.js
+++ complete-node-bootcamp-main/4-natours/starter/models/tourModels.js
@@ -1,61 +1,74 @@
 const mongoose = require('mongoose')
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty']
+    },
+    rating: {
+      type: Number,
+      default: 4.5
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price']
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date]
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty']
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 0
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description']
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image']
-  },
-  images: [String],
-  createdAt:{
-    type: Date,
-    default: Date.now()
-  },
-  startDates: [Date]  
-})
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+)
 
+// virtual properties don't get stored in the data 
+//base and cannot be accessed as fields like regular 
+// fields
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7
+})
 
 const Tour = mongoose.model('Tour', tourSchema)
 
