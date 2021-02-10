c953b1402c976e0c9e510cc83e470b1a940a23c0
pavementii@gmail.com <pavementii@gmail.com>
2021-02-10 08:23:03 -0500

    add global error handling middleware

--- complete-node-bootcamp-main/4-natours/starter/app.js
+++ complete-node-bootcamp-main/4-natours/starter/app.js
@@ -24,10 +24,25 @@ app
   })
   .use('/api/v1/tours', tourRouter)
   .use('/api/v1/users', userRouter)
-  .all('*', (req, res, next) =>{
-    res.status(404).json({
-      status:'fail',
-      message: `ain't no '${req.originalUrl}' on this server!`
+  .all('*', (req, res, next) => {
+    // res.status(404).json({
+    //   status:'fail',
+    //   message: `ain't no '${req.originalUrl}' on this server!`
+    // })
+
+    const err = new Error(`ain't no '${req.originalUrl}' on this mutha-effin server!`)
+    err.statu = 'fail'
+    err.statusCode = 404
+
+    next(err) // express assumes that any param sent with next is an error and will skip to the error handling portion of the code
+  })
+  .use((err, req, res, next) => {
+    err.statusCode = err.StatusCode || '500'
+    err.status = err.status || 'error'
+
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
     })
   })
 
