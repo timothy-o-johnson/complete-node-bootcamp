fdec8ce44cfaa7ed79f091b16ac3076c55e65fa9
Tim Johnson <tjohnson@2ndswing.com>
2021-02-23 08:24:46 -0500

    handle duplicate db fields error

--- complete-node-bootcamp-main/4-natours/starter/controllers/errorController.js
+++ complete-node-bootcamp-main/4-natours/starter/controllers/errorController.js
@@ -1,16 +1,23 @@
-const AppError = require("../utils/appError");
+const AppError = require('../utils/appError')
 
-const handleCastErrorDB = (err) => {
-  console.log('handleCastErrorDB(), err', err)
-  
+const handleCastErrorDB = err => {
+  // console.log('handleCastErrorDB(), err', err)
+
   const message = `Invalid field --> '${err.path}' : '${err.value}'.`
+
+  return new AppError(message, 400)
+}
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.keyValue.name
+  const message = `Duplicate field value: '${value}'. Bes' use anuther value-- if ya get get mah drift.`
 
   return new AppError(message, 400)
 }
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
-    status: err.status, 
+    status: err.status,
     error: err,
     message: err.message,
     stack: err.stack
@@ -41,9 +48,8 @@ const sendErrorProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   console.log('process.env.NODE_ENV', process.env.NODE_ENV)
   // console.log('JSON.stringify(err)', JSON.stringify(err))
-  // console.log('err', err)
-  console.log('err.name', err.name)
-
+  console.log('err', err)
+  // console.log('err.name', err.name)
 
   err.statusCode = err.StatusCode || '500'
   err.status = err.status || 'error'
@@ -51,10 +57,14 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res)
   } else if (process.env.NODE_ENV === 'production') {
-    let error  = {...err}
+    let error = { ...err }
     error.name = err.name
-     
+    // error.code = err.code
+
+    console.log('JSON.stringify(error)', JSON.stringify(error))
+
     if (error.name === 'CastError') error = handleCastErrorDB(error)
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error)
 
     sendErrorProd(error, res)
   }
