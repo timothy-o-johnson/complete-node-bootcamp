e26b36d46c23cabfa0d891ebe181972ad03481b2
Tim Johnson <pavementii@gmail.com>
2022-05-05 07:20:07 -0400

    implement rate limiting

--- complete-node-bootcamp-main/4-natours/starter/app.js
+++ complete-node-bootcamp-main/4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -15,6 +16,14 @@ const app = express()
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowms: 60 * 60 * 1000,
+  message: 'too many requests from this IP, please try again in 1 hour1'
+})
+
+app.use('/api', limiter)
 
 app
   .use(express.json())
