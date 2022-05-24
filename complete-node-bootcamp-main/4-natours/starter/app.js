2e31066d32457dcd61658859737eb9820d44e621
Tim Johnson <pavementii@gmail.com>
2022-05-24 07:46:05 -0400

    prevent parameter pollution

--- complete-node-bootcamp-main/4-natours/starter/app.js
+++ complete-node-bootcamp-main/4-natours/starter/app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -41,6 +42,19 @@ app
   .use(mongoSanitize())
   // data sanitization against cross site query attacks (XSS)
   .use(xss())
+  // prevent parameter pollution
+  .use(
+    hpp({
+      whitelist: [
+        'difficulty',
+        'duration',
+        'maxGroupSize',
+        'price',
+        'ratingsQuantity',
+        'ratingsAverage',        
+      ]
+    })
+  )
   // serving static files
   .use(express.static(`${__dirname}/public`))
   // helpful for testing middleware
